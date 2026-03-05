d6771d4066a1932fc4e19e54b166d0f0220050c3
ara-zo <jo91kr@gmail.com>
2026-03-05 18:46:44 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/aireview/AiReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aireview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiReviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiReviewApplication>(*args)
+}
